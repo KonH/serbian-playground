6936ad2b161867c3a0fc6b9bd7ab9ed8ed2b5874
KonH <konh@yandex.ru>
2023-11-05 15:22:08 +0100

    Fix issues

--- src/components/VerbFormTest.vue
+++ src/components/VerbFormTest.vue
@@ -23,7 +23,7 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'VerbFormTest',
-  data() {
+  data: () => {
     return {
       isRight: false,
       isWrong: false
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import { BootstrapVue3 } from 'bootstrap-vue-3'
 
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 const app = createApp(App);
 app.use(BootstrapVue3);
