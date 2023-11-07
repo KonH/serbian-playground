7f28fe5f13f549338d8592b8009148d7c430f6c7
KonH <konh@yandex.ru>
2023-11-07 18:05:50 +0100

    Minimal working prototype

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="container">
-    <VerbFormTable />
-    <VerbFormTest />
+    <div class="d-flex flex-column align-items-center" v-if="inMenu">
+      <b-button @click="openVerbTable" variant="primary" class="mb-3">Biti verb forms table</b-button>
+      <b-button @click="openVerbTest" variant="danger">Biti verb forms test</b-button>
+    </div>
+    <div v-if="inVerbFormTable" class="d-flex flex-column align-items-center">
+      <b-button @click="backToMenu" variant="secondary" class="mb-3">Back</b-button>
+      <VerbFormTable />
+    </div>
+    <div v-if="inVerbFormTest" class="d-flex flex-column align-items-center">
+      <b-button @click="backToMenu" variant="secondary" class="mb-3">Back</b-button>
+      <VerbFormTest />
+    </div>
   </div>
 </template>
 
@@ -10,8 +20,41 @@ import { defineComponent } from 'vue';
 import VerbFormTable from './components/VerbFormTable.vue';
 import VerbFormTest from './components/VerbFormTest.vue';
 
+type State = 'menu' | 'verb-form-table' | 'verb-form-test';
+
 export default defineComponent({
   name: 'App',
+  data: () => {
+    return {
+      state: 'menu' as State
+    }
+  },
+  computed: {
+    inMenu() {
+      return this.state == 'menu';
+    },
+
+    inVerbFormTable() {
+      return this.state == 'verb-form-table';
+    },
+
+    inVerbFormTest() {
+      return this.state == 'verb-form-test';
+    }
+  },
+  methods: {
+    openVerbTable() {
+      this.state = 'verb-form-table';
+    },
+
+    openVerbTest() {
+      this.state = 'verb-form-test';
+    },
+    
+    backToMenu() {
+      this.state = 'menu';
+    }
+  },
   components: {
     VerbFormTable, VerbFormTest
   }
--- src/components/VerbFormTest.vue
+++ src/components/VerbFormTest.vue
@@ -2,17 +2,18 @@
 <table class="test-table table table-bordered">
   <thead>
     <tr>
-      <th colspan="2">Ja?</th>
+      <th>{{ rightCounter }}</th>
+      <th colspan="2">{{ question }}?</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <td><b-button :variant="buttonVariant" :class="{ 'btn-success': isRight }" @click="onrightclick">sam</b-button></td>
-      <td><b-button variant="info">smo</b-button></td>
+      <td><b-button variant="info" @click="checkAnswer(0)">{{ answers[0] }}</b-button></td>
+      <td><b-button variant="info" @click="checkAnswer(1)">{{ answers[1] }}</b-button></td>
     </tr>
     <tr>
-      <td><b-button variant="info">si</b-button></td>
-      <td><b-button variant="info">su</b-button></td>
+      <td><b-button variant="info" @click="checkAnswer(2)">{{ answers[2] }}</b-button></td>
+      <td><b-button variant="info" @click="checkAnswer(3)">{{ answers[3] }}</b-button></td>
     </tr>
   </tbody>
 </table>
@@ -21,27 +22,72 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+const bitiForms: Record<string, string> = {
+  'ja': 'sam',
+  'ti': 'si',
+  'on': 'je',
+  'ona (ed)': 'je',
+  'ono': 'je',
+  'mi': 'smo',
+  'vi': 'ste',
+  'oni': 'su',
+  'one': 'su',
+  'ona (mn)': 'su'
+};
+
 export default defineComponent({
   name: 'VerbFormTest',
   data: () => {
     return {
-      isRight: false,
-      isWrong: false
-    }
-  },
-  computed: {
-    buttonVariant() {
-      return this.isRight ? 'success' : 'info';
+      question: '',
+      answers: [] as string[],
+      rightCounter: 0
     }
   },
   methods: {
-    onrightclick() {
-      console.log('right');
-      this.isRight = true;
+    checkAnswer(index: number) {
+      const correctAnswer = bitiForms[this.question];
+      if (this.answers[index] === correctAnswer) {
+        this.onRightClick();
+      } else {
+        this.onWrongClick();
+      }
     },
-    onwrongclick() {
-      this.isWrong = true;
+
+    onRightClick() {
+      this.rightCounter++;
+      this.generateNewQuestion();
+    },
+    
+    onWrongClick() {
+      this.resetCounter();
+    },
+    
+    resetCounter() {
+      this.rightCounter = 0;
+    },
+    
+    generateNewQuestion() {
+      const questions = Object.keys(bitiForms);
+      this.question = questions[Math.floor(Math.random() * questions.length)];
+      const correctAnswer = bitiForms[this.question];
+      let incorrectAnswers = Object.values(bitiForms).filter(answer => answer !== correctAnswer);
+      incorrectAnswers = this.shuffleArray(incorrectAnswers);
+      incorrectAnswers = incorrectAnswers.slice(0, 3);
+      this.answers = this.shuffleArray([correctAnswer, ...incorrectAnswers]);
+    },
+    
+    shuffleArray(array: string[]) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
     }
+  },
+  mounted() {
+    this.resetCounter();
+    this.generateNewQuestion();
   }
 });
 </script>
